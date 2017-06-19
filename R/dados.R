89271b2d831b4c21197b8e1a3dad482038736df2
pedroliman <pedrolima.n@gmail.com>
2017-06-19 12:06:22 -0300

    removendo a dependência pelo java com o readxl

--- R/dados.R
+++ R/dados.R
@@ -1,3 +1,5 @@
+
+library(readxl)
 #' Carregar Dados
 #'
 #' Esta função carrega dados que ser?o usados como Input.
@@ -8,14 +10,10 @@
 #' carregar_inputs ("Planilha_de_Inputs.xlsx")
 carregar_inputs = function (arquivo_de_inputs) {
   ## Ajustando variável de ambiente do Java
-  Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jdk1.8.0_101\\jre\\')
 
-  # Carregando os Dados
-  library(xlsx)
+  params = read_excel(arquivo_de_inputs,sheet = "Parametros")
 
-  params = read.xlsx(arquivo_de_inputs, sheetName="Parametros")
-
-  dados_projetados = read.xlsx(arquivo_de_inputs, sheetName="Dados_Projetados")
+  dados_projetados = read_excel(arquivo_de_inputs, sheet="Dados_Projetados")
 
   dados_projetados = na.omit(dados_projetados)
 
