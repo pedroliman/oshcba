47ba5a077de1f5205a79c45cf6b99c3a36c7c907
pedroliman <pedrolima.n@gmail.com>
2017-07-17 15:18:34 -0300

    Alterando local das funcoes

--- R/modelo.R
+++ R/modelo.R
@@ -1,21 +1,4 @@
-
-calcular_eventos = function(parametros) {
-
-  prefixo_inputs = oshcba_options$pref_prob_ev
-  prefixo_outputs = oshcba_options$pref_n_ev
-  eventos_k = oshcba_options$vetor_eventos_k
-  consequencias_c = oshcba_options$vetor_consequencias_c
-  vetor_inputs = paste(prefixo_inputs, eventos_k, sep = "_")
-  vetor_outputs = paste(prefixo_outputs, eventos_k, sep = "_")
-
-  parametros[vetor_outputs] = formula_nev_k(f = parametros$Funcionarios, Pev_k = parametros[vetor_inputs])
-  parametros
-
-}
-
-
-
-############# EVENTOS E CONSEQUÊNCIAS #################
+############# EVENTOS E CONSEQUENCIAS #################
 
 calcular_eventos_e_consequencias = function(parametros) {
 
@@ -59,17 +42,6 @@ formula_nev_k = function(f, Pev_k) {
 formula_ncs_j_k = function(Nev_k, Pcs_k_l) {
   round(x = Nev_k*Pcs_k_l, digits = 0)
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 ############# FALTAS #################
@@ -161,14 +133,7 @@ calcular_despesa_absenteismo = function(parametros) {
 
 
 
-
-
-
-
-
-
-
-# Funcoes nao utilizadas
+### FUNCOES NAO UTILIZADAS ####
 conferir_params = function(parametros, inputs) {
 
   mensagem = "Confira seu arquivo de dados. Voce não informou todos os parametros."
--- R/test.R
+++ R/test.R
@@ -1,17 +1,1 @@
 
-#' @export
-simular_e_gravar_resultados = function () {
-  base_folder = paste(getwd(),"resultados",as.character(Sys.time()), sep = "/")
-  resultados = simular_temp_absenteismo(modo = "completo")
-
-  ## Pode existir uma maneira mais prática de fazer isto, mas eu não encontrei:
-  write.table(resultados$Inputs$Configs, "Inputs$Configs.csv",sep=";",dec=",",row.names = FALSE)
-  write.table(resultados$Inputs$DadosProjetados, "Inputs$DadosProjetados.csv",sep=";",dec=",",row.names = FALSE)
-  write.table(resultados$Inputs$Parametros, "Inputs$Parametros.csv",sep=";",dec=",",row.names = FALSE)
-  write.table(resultados$Inputs$Cenarios, "Inputs$Cenarios.csv",sep=";",dec=",",row.names = FALSE)
-  write.table(resultados$Inputs$Custos, "Inputs$Custos.csv",sep=";",dec=",",row.names = FALSE)
-  write.table(resultados$Parametros, "Parametros.csv",sep=";",dec=",",row.names = FALSE)
-  write.table(resultados$Resultados, "Resultados.csv",sep=";",dec=",",row.names = FALSE)
-  write.table(resultados$Resultados_Descontados, "Resultados_CBR.csv",sep=";",dec=",",row.names = FALSE)
-  resultados
-}
