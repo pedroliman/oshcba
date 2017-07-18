694747ac151019a5c67a85ce25d26a4ec1c46cfb
pedroliman <pedrolima.n@gmail.com>
2017-07-18 19:05:45 -0300

    Updating tests

--- tests/testthat/testcarregar_inputs.R
+++ tests/testthat/testcarregar_inputs.R
@@ -2,7 +2,7 @@ library(testthat)
 library(oshcba)
 context("Test carregar_Inputs()")
 
-inputs = carregar_inputs("D:/dev/oshcba/data/Dados.xlsx")
+inputs = carregar_inputs("../../data/Dados.xlsx")
 
 test_that("carregar_inputs retorna lista com 7 elementos", {
   expect_equal(length(inputs), 7)
