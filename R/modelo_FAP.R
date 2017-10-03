598f08bb938aad80cf8dbd41c348c714410ee37e
pedroliman <pedrolima.n@gmail.com>
2017-10-03 16:12:49 -0300

    Calculo do FAP está muito lento

--- R/modelo_FAP.R
+++ R/modelo_FAP.R
@@ -23,86 +23,162 @@ calcular_fap = function(parametros, historico) {
 
   dindicegrav = "DenomIndiceGravidadeFAP"
 
+  # # Esta função está demorando um minuto para executar, porém preciso dos dados calculados neste formato (seja assim ou de outra forma.)
+  # for (l in 1:nrow(parametros)) {
+  #
+  #
+  #   if (parametros[l,"Ano"] == ano_inicial) {
+  #
+  #     # Primeiro e Segundo Ano do Histórico
+  #     parametros[l,"EventosFrequenciaFAP"] = rowSums(historico[1,colunas_frequencia]) + rowSums(historico[2,colunas_frequencia])
+  #
+  #     parametros[l,"FolhaSalarialMediaFAP"] = (historico[1,folha] + historico[2,folha])/2
+  #
+  #     parametros[l,"FuncionariosMedioFAP"] =  (historico[1,func] + historico[2,func])/2
+  #
+  #     parametros[l,"TurnoverGeralMedioFAP"] = (historico[1,turn] + historico[2,turn])/2
+  #
+  #     parametros[l,"CustoBeneficiosFAP_Ultimos2Anos"] = (historico[1,ctotal] + historico[2,ctotal])
+  #
+  #     parametros[l,dindicegrav] = (0.1 * (
+  #       historico[1,"NB_91"] + historico[2,"NB_91"]
+  #     ) + 0.3 * (
+  #       historico[1,"NB_92"] + historico[2,"NB_92"]
+  #     )+ 0.5 * (
+  #       historico[1,"NB_93"] + historico[2,"NB_93"]
+  #     ) + 0.1 * (
+  #       historico[1,"NB_94"] + historico[2,"NB_94"]
+  #     )
+  #     ) * 1000
+  #
+  #   } else if (parametros[l,"Ano"] == ano_inicial + 1){
+  #
+  #     # Segundo Ano do Historico e Ano Anterior
+  #     parametros[l,"EventosFrequenciaFAP"] = rowSums(historico[2,colunas_frequencia]) + rowSums(parametros[l-1,colunas_frequencia])
+  #
+  #     parametros[l,"FolhaSalarialMediaFAP"] = (historico[2,folha] + parametros[l-1,folha])/2
+  #
+  #     parametros[l,"FuncionariosMedioFAP"] = (historico[2,func] + parametros[l-1,func])/2
+  #
+  #     parametros[l,"TurnoverGeralMedioFAP"] = (historico[2,turn] + parametros[l-1,turn])/2
+  #
+  #     parametros[l,"CustoBeneficiosFAP_Ultimos2Anos"] = (historico[2,ctotal] + parametros[l-1,ctotal])
+  #
+  #     parametros[l,dindicegrav] =       (0.1 * (
+  #       parametros[l-1,"NB_91"] + historico[2,"NB_91"]
+  #     ) + 0.3 * (
+  #       parametros[l-1,"NB_92"] + historico[2,"NB_92"]
+  #     )+ 0.5 * (
+  #       parametros[l-1,"NB_93"] + historico[2,"NB_93"]
+  #     ) + 0.1 * (
+  #       parametros[l-1,"NB_94"] + historico[2,"NB_94"]
+  #     )
+  #     ) * 1000
+  #
+  #   } else {
+  #     # Dois Anos Anteriores
+  #     parametros[l,"EventosFrequenciaFAP"] = rowSums(parametros[l-1,colunas_frequencia]) + rowSums(parametros[l-2,colunas_frequencia])
+  #
+  #     parametros[l,"FolhaSalarialMediaFAP"] = (parametros[l-2,folha]+parametros[l-1,folha])/2
+  #
+  #     parametros[l,"FuncionariosMedioFAP"] = (parametros[l-2,func]+parametros[l-1,func])/2
+  #
+  #     parametros[l,"TurnoverGeralMedioFAP"] = (parametros[l-2,turn]+parametros[l-1,turn])/2
+  #
+  #     parametros[l,"CustoBeneficiosFAP_Ultimos2Anos"] =  (parametros[l-2,ctotal]+parametros[l-1,ctotal])
+  #
+  #     parametros[l,dindicegrav] =       (0.1 * (
+  #       parametros[l-1,"NB_91"] + parametros[l-2,"NB_91"]
+  #     ) + 0.3 * (
+  #       parametros[l-1,"NB_92"] + parametros[l-2,"NB_92"]
+  #     )+ 0.5 * (
+  #       parametros[l-1,"NB_93"] + parametros[l-2,"NB_93"]
+  #     ) + 0.1 * (
+  #       parametros[l-1,"NB_94"] + parametros[l-2,"NB_94"]
+  #     )
+  #     ) * 1000
+  #
+  #   }
+  #
+  # }
+
+  # Tentando substituir o For pela função do Which:
+
+  linhas_ano_inicial = which(parametros[,"Ano"] == ano_inicial)
+  linhas_segundo_ano = which(parametros[,"Ano"] == ano_inicial + 1)
+  linhas_outros_anos = which(parametros[,"Ano"] > ano_inicial + 1)
 
 
-  # Esta função está demorando um minuto para executar, porém preciso dos dados calculados neste formato (seja assim ou de outra forma.)
-  for (l in 1:nrow(parametros)) {
+  # Calculando Variáveis para os Anos Iniciais:
+  # Primeiro e Segundo Ano do Histórico
+  parametros[linhas_ano_inicial,"EventosFrequenciaFAP"] = rowSums(historico[1,colunas_frequencia]) + rowSums(historico[2,colunas_frequencia])
+
+  parametros[linhas_ano_inicial,"FolhaSalarialMediaFAP"] = (historico[1,folha] + historico[2,folha])/2
+
+  parametros[linhas_ano_inicial,"FuncionariosMedioFAP"] =  (historico[1,func] + historico[2,func])/2
+
+  parametros[linhas_ano_inicial,"TurnoverGeralMedioFAP"] = (historico[1,turn] + historico[2,turn])/2
+
+  parametros[linhas_ano_inicial,"CustoBeneficiosFAP_Ultimos2Anos"] = (historico[1,ctotal] + historico[2,ctotal])
+
+  parametros[linhas_ano_inicial,dindicegrav] = (0.1 * (
+    historico[1,"NB_91"] + historico[2,"NB_91"]
+  ) + 0.3 * (
+    historico[1,"NB_92"] + historico[2,"NB_92"]
+  )+ 0.5 * (
+    historico[1,"NB_93"] + historico[2,"NB_93"]
+  ) + 0.1 * (
+    historico[1,"NB_94"] + historico[2,"NB_94"]
+  )
+  ) * 1000
 
 
-    if (parametros[l,"Ano"] == ano_inicial) {
+  # Calculando Variáveis para o Segundo Ano:
+  parametros[linhas_segundo_ano,"EventosFrequenciaFAP"] = rowSums(historico[2,colunas_frequencia]) + rowSums(parametros[linhas_segundo_ano-1,colunas_frequencia])
 
-      # Primeiro e Segundo Ano do Histórico
-      parametros[l,"EventosFrequenciaFAP"] = rowSums(historico[1,colunas_frequencia]) + rowSums(historico[2,colunas_frequencia])
+  parametros[linhas_segundo_ano,"FolhaSalarialMediaFAP"] = (historico[2,folha] + parametros[linhas_segundo_ano-1,folha])/2
 
-      parametros[l,"FolhaSalarialMediaFAP"] = (historico[1,folha] + historico[2,folha])/2
+  parametros[linhas_segundo_ano,"FuncionariosMedioFAP"] = (historico[2,func] + parametros[linhas_segundo_ano-1,func])/2
 
-      parametros[l,"FuncionariosMedioFAP"] =  (historico[1,func] + historico[2,func])/2
+  parametros[linhas_segundo_ano,"TurnoverGeralMedioFAP"] = (historico[2,turn] + parametros[linhas_segundo_ano-1,turn])/2
 
-      parametros[l,"TurnoverGeralMedioFAP"] = (historico[1,turn] + historico[2,turn])/2
+  parametros[linhas_segundo_ano,"CustoBeneficiosFAP_Ultimos2Anos"] = (historico[2,ctotal] + parametros[linhas_segundo_ano-1,ctotal])
 
-      parametros[l,"CustoBeneficiosFAP_Ultimos2Anos"] = (historico[1,ctotal] + historico[2,ctotal])
+  parametros[linhas_segundo_ano,dindicegrav] =       (0.1 * (
+    parametros[linhas_segundo_ano-1,"NB_91"] + historico[2,"NB_91"]
+  ) + 0.3 * (
+    parametros[linhas_segundo_ano-1,"NB_92"] + historico[2,"NB_92"]
+  )+ 0.5 * (
+    parametros[linhas_segundo_ano-1,"NB_93"] + historico[2,"NB_93"]
+  ) + 0.1 * (
+    parametros[linhas_segundo_ano-1,"NB_94"] + historico[2,"NB_94"]
+  )
+  ) * 1000
 
-      parametros[l,dindicegrav] = (0.1 * (
-        historico[1,"NB_91"] + historico[2,"NB_91"]
-      ) + 0.3 * (
-        historico[1,"NB_92"] + historico[2,"NB_92"]
-      )+ 0.5 * (
-        historico[1,"NB_93"] + historico[2,"NB_93"]
-      ) + 0.1 * (
-        historico[1,"NB_94"] + historico[2,"NB_94"]
-      )
-      ) * 1000
 
-    } else if (parametros[l,"Ano"] == ano_inicial + 1){
+  # Calculando Variáveis para os Demais Anos:
+  # Segundo Ano do Historico e Ano Anterior
+  parametros[linhas_outros_anos,"EventosFrequenciaFAP"] = rowSums(parametros[linhas_outros_anos-1,colunas_frequencia]) + rowSums(parametros[linhas_outros_anos-2,colunas_frequencia])
 
-      # Segundo Ano do Historico e Ano Anterior
-      parametros[l,"EventosFrequenciaFAP"] = rowSums(historico[2,colunas_frequencia]) + rowSums(parametros[l-1,colunas_frequencia])
+  parametros[linhas_outros_anos,"FolhaSalarialMediaFAP"] = (parametros[linhas_outros_anos-2,folha]+parametros[linhas_outros_anos-1,folha])/2
 
-      parametros[l,"FolhaSalarialMediaFAP"] = (historico[2,folha] + parametros[l-1,folha])/2
+  parametros[linhas_outros_anos,"FuncionariosMedioFAP"] = (parametros[linhas_outros_anos-2,func]+parametros[linhas_outros_anos-1,func])/2
 
-      parametros[l,"FuncionariosMedioFAP"] = (historico[2,func] + parametros[l-1,func])/2
+  parametros[linhas_outros_anos,"TurnoverGeralMedioFAP"] = (parametros[linhas_outros_anos-2,turn]+parametros[linhas_outros_anos-1,turn])/2
 
-      parametros[l,"TurnoverGeralMedioFAP"] = (historico[2,turn] + parametros[l-1,turn])/2
+  parametros[linhas_outros_anos,"CustoBeneficiosFAP_Ultimos2Anos"] =  (parametros[linhas_outros_anos-2,ctotal]+parametros[linhas_outros_anos-1,ctotal])
 
-      parametros[l,"CustoBeneficiosFAP_Ultimos2Anos"] = (historico[2,ctotal] + parametros[l-1,ctotal])
+  parametros[linhas_outros_anos,dindicegrav] =       (0.1 * (
+    parametros[linhas_outros_anos-1,"NB_91"] + parametros[linhas_outros_anos-2,"NB_91"]
+  ) + 0.3 * (
+    parametros[linhas_outros_anos-1,"NB_92"] + parametros[linhas_outros_anos-2,"NB_92"]
+  )+ 0.5 * (
+    parametros[linhas_outros_anos-1,"NB_93"] + parametros[linhas_outros_anos-2,"NB_93"]
+  ) + 0.1 * (
+    parametros[linhas_outros_anos-1,"NB_94"] + parametros[linhas_outros_anos-2,"NB_94"]
+  )
+  ) * 1000
 
-      parametros[l,dindicegrav] =       (0.1 * (
-        parametros[l-1,"NB_91"] + historico[2,"NB_91"]
-      ) + 0.3 * (
-        parametros[l-1,"NB_92"] + historico[2,"NB_92"]
-      )+ 0.5 * (
-        parametros[l-1,"NB_93"] + historico[2,"NB_93"]
-      ) + 0.1 * (
-        parametros[l-1,"NB_94"] + historico[2,"NB_94"]
-      )
-      ) * 1000
-
-    } else {
-      # Dois Anos Anteriores
-      parametros[l,"EventosFrequenciaFAP"] = rowSums(parametros[l-1,colunas_frequencia]) + rowSums(parametros[l-2,colunas_frequencia])
-
-      parametros[l,"FolhaSalarialMediaFAP"] = (parametros[l-2,folha]+parametros[l-1,folha])/2
-
-      parametros[l,"FuncionariosMedioFAP"] = (parametros[l-2,func]+parametros[l-1,func])/2
-
-      parametros[l,"TurnoverGeralMedioFAP"] = (parametros[l-2,turn]+parametros[l-1,turn])/2
-
-      parametros[l,"CustoBeneficiosFAP_Ultimos2Anos"] =  (parametros[l-2,ctotal]+parametros[l-1,ctotal])
-
-      parametros[l,dindicegrav] =       (0.1 * (
-        parametros[l-1,"NB_91"] + parametros[l-2,"NB_91"]
-      ) + 0.3 * (
-        parametros[l-1,"NB_92"] + parametros[l-2,"NB_92"]
-      )+ 0.5 * (
-        parametros[l-1,"NB_93"] + parametros[l-2,"NB_93"]
-      ) + 0.1 * (
-        parametros[l-1,"NB_94"] + parametros[l-2,"NB_94"]
-      )
-      ) * 1000
-
-    }
-
-  }
 
 
   # Calculando Índices e Percentis do FAP
@@ -159,21 +235,50 @@ calcular_fap = function(parametros, historico) {
 
 
   # Calculando Finalmente o Imposto de modo Recursivo
-  for (l in 1:nrow(parametros)) {
 
-    # Ajustando o Bônus quando o FAP é menor do que 1
-    parametros[l,"FAP"] = if (parametros[l,"FAPSemAjuste"] < 1) {parametros[l,"FAPSemAjuste"] * 0.5 + 0.5} else {parametros[l,"FAPSemAjuste"]}
 
-    # RAT Ajustado
-    parametros[l,"RATAjustado"] = parametros[l,"FAP"] * parametros[l,"RATTabela"]
+  # Ajustando o Bônus quando o FAP é menor do que 1
+  linhas_fap_men_um = which(parametros[,"FAPSemAjuste"] < 1)
+  linhas_fap_maior_igual_um = which(!(parametros[,"FAPSemAjuste"] < 1))
 
-    parametros[l,"DespesaFAP"] =
-      if(parametros[l,"Ano"] == ano_inicial) {
-        -historico[2,"RATAjustado"] * parametros[l,"FolhadePagamento"]
-      } else {
-          -parametros[l-1,"RATAjustado"] * parametros[l,"FolhadePagamento"]
-        }
-  }
+  parametros[linhas_fap_men_um,"FAP"] = parametros[linhas_fap_men_um,"FAPSemAjuste"] * 0.5 + 0.5
+
+  parametros[linhas_fap_maior_igual_um,"FAP"] = parametros[linhas_fap_maior_igual_um,"FAPSemAjuste"]
+
+
+  # RAT Ajustado
+  parametros["RATAjustado"] = parametros["FAP"] * parametros["RATTabela"]
+
+  # Despesa FAP, Ano Inicial
+  parametros[linhas_ano_inicial,"DespesaFAP"] =  -historico[2,"RATAjustado"] * parametros[linhas_ano_inicial,"FolhadePagamento"]
+
+
+  # Despesa FAP, Outros Anos
+  linhas_sem_anos_niciais = c(linhas_outros_anos, linhas_segundo_ano)
+
+
+  parametros[linhas_sem_anos_niciais,"DespesaFAP"] =  -parametros[linhas_sem_anos_niciais-1,"RATAjustado"] * parametros[linhas_sem_anos_niciais,"FolhadePagamento"]
+
+
+  # Funcao Antiga com FOR.
+  # for (l in 1:nrow(parametros)) {
+  #
+  #   # Ajustando o Bônus quando o FAP é menor do que 1
+  #   parametros[l,"FAP"] = if (parametros[l,"FAPSemAjuste"] < 1) {parametros[l,"FAPSemAjuste"] * 0.5 + 0.5} else {parametros[l,"FAPSemAjuste"]}
+  #
+  #   # RAT Ajustado
+  #   parametros[l,"RATAjustado"] = parametros[l,"FAP"] * parametros[l,"RATTabela"]
+  #
+  #   parametros[l,"DespesaFAP"] =
+  #     if(parametros[l,"Ano"] == ano_inicial) {
+  #       -historico[2,"RATAjustado"] * parametros[l,"FolhadePagamento"]
+  #     } else {
+  #         -parametros[l-1,"RATAjustado"] * parametros[l,"FolhadePagamento"]
+  #       }
+  # }
+
+
+
 
  parametros
 
