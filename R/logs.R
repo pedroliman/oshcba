a3ee43f5f0472c0532eb2f7c48e354451f052bca
pedroliman <pedrolima.n@gmail.com>
2017-11-03 14:51:57 -0200

    Alterando funcoes de log - opção para salvar ou não os logs como csv na pasta atual da calculadora.

--- R/logs.R
+++ R/logs.R
@@ -2,12 +2,18 @@
 
 # Funcoes Simples para obter o controle de logs internos da calculadora.
 # Criando uma variável global para os logs como um data.fram
+#' oshcba.iniciar_log
+#' Esta funcao inicia o vetor de logs da calculadora.
 oshcba.iniciar_log = function(){
   oshcba.log_calculadora <<- vector(mode = "character")
   oshcba.adicionar_log("### CALCULADORA SESI / GMAP | UNISINOS - LOGS DE EXECUÇÃO ###")
   oshcba.adicionar_log(paste("Versão:",packageDescription("oshcba")$Version))
 }
 
+#' oshcba.adicionar_log
+#'
+#' @param string character com o texto a adicionar ao vetor de logs
+#' @param vetor_log o vetor de log existente
 oshcba.adicionar_log = function(string, vetor_log = oshcba.log_calculadora) {
 
   # Adicionar Data e Hora para o Log:
@@ -21,7 +27,13 @@ oshcba.adicionar_log = function(string, vetor_log = oshcba.log_calculadora) {
 
 }
 
-oshcba.parar_execucao = function(string, vetor_log = oshcba.log_calculadora) {
+#' oshcba.parar_execucao
+#'
+#' Esta funcao pode ser usada para parar a execução da biblioteca salvando um arquivo com os logs na pasta atual.
+#' @param string character com o texto a adicionar ao vetor de logs
+#' @param vetor_log o vetor de log existente
+#'
+oshcba.parar_execucao = function(string, vetor_log = oshcba.log_calculadora, salvar_logs_csv = FALSE) {
   # Adicionar Data e Hora para o Log:
   string = paste(Sys.time(),string, sep = " - ")
 
@@ -32,7 +44,9 @@ oshcba.parar_execucao = function(string, vetor_log = oshcba.log_calculadora) {
   df_logs = oshcba.obter_log()
 
   # Escrever os logs em um csv na pasta atual.
-  write.csv2(df_logs, file = "logs_execucao.csv")
+  if(salvar_logs_csv) {
+    write.csv2(df_logs, file = "logs_execucao.csv")
+  }
 
   stop(string)
 
