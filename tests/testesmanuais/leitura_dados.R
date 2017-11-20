3e4dec4f071f4a0cb253a82eb611460199d1fe0c
Pedro Nascimento de Lima <pedrolima.n@gmail.com>
2017-11-20 18:54:51 -0200

    alterando arquivo do template

--- tests/testesmanuais/leitura_dados.R
+++ tests/testesmanuais/leitura_dados.R
@@ -2,6 +2,7 @@
 # Atualizar a biblioteca:
 devtools::install_github("pedroliman/oshcba", auth_token = "a99dc0254a0e73b9e08fca868ab1df6e32877153")
 library(oshcba)
-library(dplyr)
+
+list_inputs = oshcba::obter_inputs_list_dados_tratados()
 resultados = simular_cba(ArquivoInputs = obter_inputs_list_dados_tratados() , tipo_input = "list")
 rmarkdown::render("/home/pedro/Documents/dev/oshcba/R/relatorio.Rmd", encoding = "UTF-8")
