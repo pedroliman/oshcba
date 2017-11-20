77737edcb0bb0f597dfda896cca2c32b0215f8f6
Pedro Nascimento de Lima <pedrolima.n@gmail.com>
2017-11-20 18:14:54 -0200

    mudando leitura

--- tests/testesmanuais/leitura_dados.R
+++ tests/testesmanuais/leitura_dados.R
@@ -3,5 +3,5 @@
 devtools::install_github("pedroliman/oshcba", auth_token = "a99dc0254a0e73b9e08fca868ab1df6e32877153")
 library(oshcba)
 library(dplyr)
-resultados = simular_cba(ArquivoInputs = obter_inputs_list_dados_tratados() , tipo_input = "list", verificar_inputs = FALSE)
+resultados = simular_cba(ArquivoInputs = obter_inputs_list_dados_tratados() , tipo_input = "list")
 rmarkdown::render("/home/pedro/Documents/dev/oshcba/R/relatorio.Rmd", encoding = "UTF-8")
